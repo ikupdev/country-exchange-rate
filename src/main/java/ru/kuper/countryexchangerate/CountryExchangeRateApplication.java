25eef12cb015d02cddf1de04c2ac81d94a351300
Ilya Kupriyanov <ikupdev@gmail.com>
2020-08-11 15:36:49 +0300

    initial commit

---
+++ src/main/java/ru/kuper/countryexchangerate/CountryExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package ru.kuper.countryexchangerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryExchangeRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryExchangeRateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kuper/countryexchangerate/CountryExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuper.countryexchangerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryExchangeRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
